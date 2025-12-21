08cfa3429884d87c3776855b2df7b6d5f011b2f6
Stanley Kambona <70863585+stan646@users.noreply.github.com>
2025-12-21 17:33:04 +0300

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,21 +1,20 @@
-const showPost = JSON.parse(localStorage.getItem('post'));
-
-document.getElementById('showCaption').innerHTML = showPost.captionPost;
-const postTime = document.getElementById('post-date');
-
-if (showPost.time <= 12) {
-    postTime.innerHTML = `${showPost.time} AM`
-} else {
-    postTime.innerHTML = `${showPost.time} PM`
+function postDetails (){
+    const showPost = JSON.parse(localStorage.getItem('userPost'));
+    document.getElementById('showCaption').innerHTML = showPost.postData.captionPost;
+    const postTime = document.getElementById('post-date');
+    postTime.innerHTML = showPost.postDateInfo.time;   
 }
+postDetails();
 
 
-document.getElementById('like-btn').addEventListener('click', function(){
-    const showLikes = document.getElementById('likes');
-    showPost.likes++;
-    if (showPost.likes >= 1000) {
-        showLikes.innerHTML = `${showPost.likes}k`;
-    } else {
-        showLikes.innerHTML = showPost.likes;
-    }
-});
+
+
+// document.getElementById('like-btn').addEventListener('click', function(){
+//     const showLikes = document.getElementById('likes');
+//     showPost.likes++;
+//     if (showPost.likes >= 1000) {
+//         showLikes.innerHTML = `${showPost.likes}k`;
+//     } else {
+//         showLikes.innerHTML = showPost.likes;
+//     }
+// });
